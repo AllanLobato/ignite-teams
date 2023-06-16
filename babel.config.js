b0bc51621550a3035ed4ae7e331cf2bb263a51a5
AllanLobato <allandonizetti@gmail.com>
2023-06-16 14:56:37 -0300

    build: Initial Configuration

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,22 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    "plugins": [
+      [
+      "module-resolver", 
+      {
+        "root": ["./src"],
+        alias: {
+          "@components": "./src/components",
+          "@screens": "./src/screens",
+          "@assets": "./src/assets",
+          "@utils": "./src/utils",
+          "@storage": "./src/storage",
+          "@theme": "./src/theme",
+          "@routes": "./src/routes",
+        },
+      },
+    ],
+    ],
   };
 };
